643eb99844e38cc00392880d89f13693f4c1e593
Grace Ariel <madyagra@gmail.com>
2024-07-22 22:06:43 +0800

    add welcome header

--- app.vue
+++ app.vue
@@ -1,6 +1,17 @@
 <template>
   <div>
-    <NuxtRouteAnnouncer />
-    <NuxtWelcome />
+    <section class="bg-white dark:bg-gray-900">
+      <div class="flex py-8 px-4 mx-auto max-w-screen-xl text-center lg:py-16">
+        <img class="rounded-full w-36 h-36" src="/assets/avatar.png" alt="Extra large avatar">
+        <div class="w-6/12">
+          <h1 class="mb-4 text-4xl font-extrabold tracking-tight leading-none text-gray-900 md:text-5xl lg:text-6xl dark:text-white">
+            Hello World!
+          </h1>
+          <p class="mb-8 text-lg font-normal text-gray-500 lg:text-xl sm:px-16 lg:px-48 dark:text-gray-400">
+            Explore the pages below to learn more about me
+          </p>
+        </div>
+      </div>
+    </section>
   </div>
 </template>
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,11 @@
 /** @type {import('tailwindcss').Config} */
 export default {
   content: [
+    "./components/**/*.{js,vue,ts}",
+    "./layouts/**/*.vue",
+    "./pages/**/*.vue",
+    "./plugins/**/*.{js,ts}",
+    "./nuxt.config.{js,ts}",
     "./node_modules/flowbite/**/*.{js,ts}"
   ],
   theme: {
