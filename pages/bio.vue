21369b24cd799027c275087ee85f0666daffdea7
Grace Ariel <madyagra@gmail.com>
2024-07-23 10:56:49 +0800

    add bio page

---
+++ pages/bio.vue
@@ -0,0 +1,22 @@
+<template>
+  <section>
+    <div class="p-16">
+      <h1 class="mb-4 text-4xl font-extrabold tracking-tight leading-none text-gray-900 md:text-5xl lg:text-6xl dark:text-white">
+        Hi! I'm Grace Ariel.
+      </h1>
+      <p>
+        I'm a software engineer specializing in web development.
+      </p>
+    </div>
+  </section>
+  <section>
+    <img class="w-36" v-for="language in languages" :src="language">
+    <img class="w-36" v-for="framework in backendFrameworks" :src="framework">
+    <img class="w-36" v-for="cloudService in cloud" :src="cloudService">
+  </section>
+</template>
+<script setup>
+  const languages = ['golang.png']
+  const backendFrameworks = ['rails.png', 'spring.png']
+  const cloud = ['aws.png']
+</script>
--- pages/index.vue
+++ pages/index.vue
@@ -1,16 +1,27 @@
 <template>
   <div>
     <section class="bg-white dark:bg-gray-900">
-      <div class="flex py-8 px-4 mx-auto max-w-screen-xl text-center lg:py-16">
+      <div class="flex py-8 px-4 mx-auto w-fit text-center lg:py-16">
         <img class="rounded-full w-36 h-36" src="/assets/avatar.png" alt="Extra large avatar">
-        <div class="w-6/12">
+        <div class="">
           <h1 class="mb-4 text-4xl font-extrabold tracking-tight leading-none text-gray-900 md:text-5xl lg:text-6xl dark:text-white">
             Hello World!
           </h1>
-          <p class="mb-8 text-lg font-normal text-gray-500 lg:text-xl sm:px-16 lg:px-48 dark:text-gray-400">
+          <p class="mb-8 text-lg font-normal text-gray-500 lg:text-xl px-16 dark:text-gray-400">
             Explore the pages below to learn more about me
           </p>
         </div>
+      </div>
+      <div>
+        <NuxtLink to="/bio">
+          Bio
+        </NuxtLink>
+        <NuxtLink to="/projects">
+          Projects
+        </NuxtLink>
+        <NuxtLink to="/blog">
+          Blog
+        </NuxtLink>
       </div>
     </section>
   </div>
