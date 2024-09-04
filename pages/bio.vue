29961f1d2080b14ea7f39f423014eca566b07dc0
Grace Ariel <madyagra@gmail.com>
2024-09-04 15:11:18 +0800

    add more bio info

--- pages/bio.vue
+++ pages/bio.vue
@@ -18,9 +18,18 @@
   <WorkExperienceBio />
   <section class="p-12">
     <h2 class="text-4xl mb-4">Education</h2>
+    <ul>
+      <li>Murdoch University, Singapore</li>
+      <li>Kaplan Diploma in Information Technology, Singapore</li>
+    </ul>
   </section>
   <section class="p-12">
     <h2 class="text-4xl mb-4">Awards/Certifications</h2>
+    <ul>
+      <li>AWS Solutions Architect Associate (SAA-C03)</li>
+      <li>Academic Merit Scholarship Award</li>
+      <li>Full Kaplan Diploma Scholarship</li>
+    </ul>
   </section>
   <section class="p-12">
     <h2 class="text-4xl mb-4">Socials</h2>
