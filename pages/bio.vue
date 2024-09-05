aa864725bc45dab4345f5a8fcd694e22234242df
Grace Ariel <madyagra@gmail.com>
2024-09-05 14:23:32 +0800

    add bio

--- pages/bio.vue
+++ pages/bio.vue
@@ -6,6 +6,28 @@
       </h1>
       <p>
         I'm a software engineer specializing in web development.
+        I moved to Singapore in 2016, where I studied computer science and cyber forensics.
+        Three years later, I began my career as a software engineer at a startup focused on automating logistics operations.
+      </p>
+      <p>
+        This experience was a gateway into the professional world, allowing me to grow both personally and professionally.
+        In the collaborative and supportive environment of a startup, I learned how to work effectively in a team, communicate ideas and arrive at solutions,
+        all while pursuing my own career goals and contributing to the company's success.
+      </p>
+      <p>
+        The challenges brought on by the pandemic taught me adaptability, resilience, and the importance of compromise and understanding during tough times.
+        Additionally, I gained valuable insights into the logistics and supply chain industry and how our startup fit into the broader market.
+      </p>
+      <p>
+        A key milestone in my career was joining Sephora SEA, where I became part of a diverse and dynamic community.
+        Sephora's commitment to beauty, diversity, and inclusion is reflected in its workplace culture and the way it values its employees.
+        I had the opportunity to work with a large technology team of 60+ people, learn from inspiring colleagues, and engage in social events.
+        As a member of the ERP squad, we successfully transformed Sephora into an omni-channel retailer, enhancing its ability to offer a seamless and personalized shopping experience.
+      </p>
+      <p>
+        I'm grateful to have a career that allows me to do the work I care about, gives me the freedom to be myself,
+        and fosters continuous learning. I am always seeking opportunities that align with my values and support both my professional growth and my personal purpose of contributing to society.
+        If you'd like to collaborate, feel free to reach out - it would be an honor to work with you.
       </p>
       <p>
         Download my resume:
