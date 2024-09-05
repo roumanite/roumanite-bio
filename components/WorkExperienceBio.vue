b7674de4b86c06e3f2af51c3232e7e48d7435608
Grace Ariel <madyagra@gmail.com>
2024-09-05 18:26:16 +0800

    extract out timeline

--- components/WorkExperienceBio.vue
+++ components/WorkExperienceBio.vue
@@ -1,58 +1,46 @@
 <template>
   <section class="p-12">
     <h2 class="text-4xl mb-4">Work Experience</h2>
-    <ol class="items-center sm:flex">
-        <li class="relative mb-6 sm:mb-0">
-            <div class="flex items-center">
-                <div class="z-10 flex items-center justify-center w-6 h-6 bg-blue-100 rounded-full ring-0 ring-white dark:bg-blue-900 sm:ring-8 dark:ring-gray-900 shrink-0">
-                    <svg class="w-2.5 h-2.5 text-blue-800 dark:text-blue-300" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="currentColor" viewBox="0 0 20 20">
-                        <path d="M20 4a2 2 0 0 0-2-2h-2V1a1 1 0 0 0-2 0v1h-3V1a1 1 0 0 0-2 0v1H6V1a1 1 0 0 0-2 0v1H2a2 2 0 0 0-2 2v2h20V4ZM0 18a2 2 0 0 0 2 2h16a2 2 0 0 0 2-2V8H0v10Zm5-8h10a1 1 0 0 1 0 2H5a1 1 0 0 1 0-2Z"/>
-                    </svg>
-                </div>
-                <div class="hidden sm:flex w-full bg-gray-200 h-0.5 dark:bg-gray-700"></div>
-            </div>
-            <div class="mt-3 sm:pe-8">
-                <h3 class="text-lg font-semibold text-gray-900 dark:text-white">VersaFleet</h3>
-                <time class="block mb-2 text-sm font-normal leading-none text-gray-400 dark:text-gray-500">October 2019 - April 2022</time>
-                <p class="text-base font-normal text-gray-500 dark:text-gray-400">
-                  Job description
-                </p>
-            </div>
-        </li>
-        <li class="relative mb-6 sm:mb-0">
-            <div class="flex items-center">
-                <div class="z-10 flex items-center justify-center w-6 h-6 bg-blue-100 rounded-full ring-0 ring-white dark:bg-blue-900 sm:ring-8 dark:ring-gray-900 shrink-0">
-                    <svg class="w-2.5 h-2.5 text-blue-800 dark:text-blue-300" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="currentColor" viewBox="0 0 20 20">
-                        <path d="M20 4a2 2 0 0 0-2-2h-2V1a1 1 0 0 0-2 0v1h-3V1a1 1 0 0 0-2 0v1H6V1a1 1 0 0 0-2 0v1H2a2 2 0 0 0-2 2v2h20V4ZM0 18a2 2 0 0 0 2 2h16a2 2 0 0 0 2-2V8H0v10Zm5-8h10a1 1 0 0 1 0 2H5a1 1 0 0 1 0-2Z"/>
-                    </svg>
-                </div>
-                <div class="hidden sm:flex w-full bg-gray-200 h-0.5 dark:bg-gray-700"></div>
-            </div>
-            <div class="mt-3 sm:pe-8">
-                <h3 class="text-lg font-semibold text-gray-900 dark:text-white">GotSurge</h3>
-                <time class="block mb-2 text-sm font-normal leading-none text-gray-400 dark:text-gray-500">October 2019 - April 2022</time>
-                <p class="text-base font-normal text-gray-500 dark:text-gray-400">
-                  Job description
-                </p>
-            </div>
-        </li>
-        <li class="relative mb-6 sm:mb-0">
-            <div class="flex items-center">
-                <div class="z-10 flex items-center justify-center w-6 h-6 bg-blue-100 rounded-full ring-0 ring-white dark:bg-blue-900 sm:ring-8 dark:ring-gray-900 shrink-0">
-                    <svg class="w-2.5 h-2.5 text-blue-800 dark:text-blue-300" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="currentColor" viewBox="0 0 20 20">
-                        <path d="M20 4a2 2 0 0 0-2-2h-2V1a1 1 0 0 0-2 0v1h-3V1a1 1 0 0 0-2 0v1H6V1a1 1 0 0 0-2 0v1H2a2 2 0 0 0-2 2v2h20V4ZM0 18a2 2 0 0 0 2 2h16a2 2 0 0 0 2-2V8H0v10Zm5-8h10a1 1 0 0 1 0 2H5a1 1 0 0 1 0-2Z"/>
-                    </svg>
-                </div>
-                <div class="hidden sm:flex w-full bg-gray-200 h-0.5 dark:bg-gray-700"></div>
-            </div>
-            <div class="mt-3 sm:pe-8">
-                <h3 class="text-lg font-semibold text-gray-900 dark:text-white">Sephora SEA</h3>
-                <time class="block mb-2 text-sm font-normal leading-none text-gray-400 dark:text-gray-500">April 2022 - present</time>
-                <p class="text-base font-normal text-gray-500 dark:text-gray-400">
-                  Job description
-                </p>
-            </div>
-        </li>
+    <ol class="sm:flex">
+      <li class="relative mb-6 sm:mb-0" v-for="experience in experiences">
+        <TimelineHeader />
+        <div class="mt-3 sm:pe-8">
+          <h3 class="text-lg font-semibold text-gray-900 dark:text-white">{{ experience.name }}</h3>
+          <time class="block mb-2 text-sm font-normal leading-none text-gray-400 dark:text-gray-500">{{ experience.date }}</time>
+          <section>
+            <ul>
+              <li v-for="point in experience.points">{{ point }}</li>
+            </ul>
+          </section>
+        </div>
+      </li>
     </ol>
   </section>
 </template>
+<script setup>
+  const experiences = [
+    {
+      name: 'VersaFleet',
+      date: 'October 2019 - April 2022',
+      points: [
+        'Maintain legacy monolithic application that solves vehicle routing problem and provides a command center for brand owners and transporters.',
+        'Communicate with customer experience team to understand the problems any client was facing and solve the bugs by gathering more hints through Sentry and Datadog',
+        'Stack: Ruby on Rails, PostgreSQL, Redis, React, Redux, React Native'
+      ]
+    },
+    {
+      name: 'GotSurge',
+      date: 'October 2019 - April 2022',
+      points: [
+        'Job description'
+      ]
+    },
+    {
+      name: 'Sephora SEA',
+      date: 'April 2022 - present',
+      points: [
+        'Job description'
+      ]
+    }
+  ]
+</script>
